f2f76fdf916351171682ea7335e6571e389b0417
Yash-Raj-5424 <myash3499@gmail.com>
2026-03-24 01:17:28 +0530

    implemented generate response from gemini

---
+++ aiservice/src/main/java/com/velocity/aiservice/services/GeminiService.java
@@ -0,0 +1,45 @@
+package com.velocity.aiservice.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class GeminiService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiAPIUrl;
+    @Value("${gemini.api.key}")
+    private String geminiAPIKey;
+
+    public GeminiService(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.build();
+    }
+
+    public String generateRecommendations(String query){
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[]{
+                        Map.of("parts" , new Object[]{
+                                Map.of("text", query)
+                        })
+                }
+        );
+
+        String response = webClient.post()
+                .uri(geminiAPIUrl)
+                .header("Content-Type", "application/json")
+                .header("X-goog-api-key", geminiAPIKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return response;
+    }
+}
